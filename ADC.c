0b9a7e4be523e3c259e1d71397d957659948e03a
Sameer Bibikar <sameerbibikar@gmail.com>
2017-04-18 19:01:41 -0500

    Add ADC files from lab9

--- ADC.c
+++ ADC.c
@@ -1,2 +1,50 @@
-// put implementations for functions, explain how it works
-// put your names here, date
+// ADC.c
+// Runs on LM4F120/TM4C123
+// Provide functions that initialize ADC0
+// Last Modified: 4/10/2016
+// Student names: Sneha Pendharkar, Sameer Bibikar
+// Last modification date: change this to the last modification date or look very silly
+
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+// ADC initialization function 
+// Input: none
+// Output: none
+void ADC_Init(void){
+  volatile uint32_t delay;
+  SYSCTL_RCGCGPIO_R |= 0x10;
+  while((SYSCTL_PRGPIO_R&0x10) == 0){};
+  GPIO_PORTE_DIR_R &= ~0x10; 
+  GPIO_PORTE_AFSEL_R |= 0x10; 
+  GPIO_PORTE_DEN_R &= ~0x10;   
+  GPIO_PORTE_AMSEL_R |= 0x10;   
+  SYSCTL_RCGCADC_R |= 0x01;      
+  delay = 42;
+  delay = 43;
+  delay = 44;
+  delay = 45;
+  ADC0_PC_R = 0x01;               
+  ADC0_SSPRI_R = 0x0123;         
+  ADC0_ACTSS_R &= ~0x0008;      
+  ADC0_EMUX_R &= ~0xF000;      
+  ADC0_SSMUX3_R = (ADC0_SSMUX3_R&0xFFFFFFF0)+9; 
+  ADC0_SSCTL3_R = 0x0006;     
+  ADC0_IM_R &= ~0x0008;      
+  ADC0_ACTSS_R |= 0x0008;   
+}
+
+//------------ADC_In------------
+// Busy-wait Analog to digital conversion
+// Input: none
+// Output: 12-bit result of ADC conversion
+uint32_t ADC_In(void){  
+  unsigned long result;
+  ADC0_PSSI_R = 0x0008;            // 1) initiate SS3
+  while((ADC0_RIS_R&0x08)==0){};   // 2) wait for conversion done
+  result = ADC0_SSFIFO3_R&0xFFF;   // 3) read result
+  ADC0_ISC_R = 0x0008;             // 4) acknowledge completion
+  return result;
+}
+
+
