a513d8a8d96ff528b73cee6616719028c648cd22
Sameer Bibikar <sameerbibikar@gmail.com>
2017-04-19 17:12:28 -0500

    Add comments to sound lookup tables

--- sound_lookup.h
+++ sound_lookup.h
@@ -1,7 +1,11 @@
 #include <stdint.h>
 
+// note_lookup contains Systick values required to produce the pitch given by the index
+// as a MIDI note number.
 const extern uint32_t note_lookup[];
 
+// sine_lookup6 contains lookup values for a 6-bit DAC
 const extern uint8_t sine_lookup6[];
 
+// sine_lookup4 contains lookup values for a 4-bit DAC
 const extern uint8_t sine_lookup4[];
